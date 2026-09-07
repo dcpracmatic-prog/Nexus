3eb3860dac6060002bea037b6d4208c136ea126e
DC PRACMATIC <289835903+dcpracmatic-prog@users.noreply.github.com>
2026-09-07 03:24:42 -0600

    chore: organize Nexus repository structure

---
+++ runtime/index.ts
@@ -0,0 +1,26 @@
+export * from "./types";
+export { assessIntent } from "./intent/assessor";
+export { gateOperation } from "./policy/commandGate";
+export { arbitrate } from "./logic";
+export { evaluateGrant } from "./authorization/resourceAuthority";
+export { createExperiment } from "./experiments/experiment";
+export { hashText } from "./audit/trace";
+export { NEXUS_CONSTITUTION, evaluateConstitution, reviewAgainstConstitution } from "./constitution";
+export { analyzeEmbeddedCode } from "./constitution/embeddedCodeAnalysis";
+export { evaluateExit } from "./vision";
+export { canPromote } from "./promotion";
+export { requestRelease } from "./promotion/releaseGate";
+export { analyzeExitArtifact } from "./release/analyzer";
+export type { ExitReport, ExitEvidence } from "./release/analyzer";
+export { evaluateCapability, DEFAULT_CAPABILITIES } from "./capabilities";
+export type { ExternalCapability, CapabilityOperation, CapabilityDecision, CapabilityRequest } from "./capabilities";
+export { createEphemeralWorkspace, authorizeTabAction, createExplicitTransfer } from "./isolation";
+export type { TabWorkspace, ExplicitTransfer, TabActionResult } from "./isolation";
+
+export { evaluateActionGate } from "./actionGate";
+export type { ActionGateRequest, ActionGateDecision } from "./actionGate";
+export { createSandboxSession, getSandboxSession, destroySandboxSession, cleanupExpiredSandboxSessions, executeSandboxOperation } from "./sandbox";
+export type { SandboxSession, SandboxResult, SandboxOperation, SandboxPolicy } from "./sandbox";
+export { resolveSecretReference } from "./secrets";
+export type { SecretReference } from "./secrets";
+
